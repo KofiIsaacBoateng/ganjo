d27d608474faa5016a9f97eaa7efa5455209eb64
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2026-03-28 03:28:54 +0000

    first half of art section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   useGSAP(() => {
     const tween = gsap.timeline({
       scrollTrigger: {
-        trigger: ".nav",
+        trigger: "nav",
         start: "bottom top",
       },
     });
@@ -16,7 +16,7 @@ const Navbar = () => {
       { backgroundColor: "transparent" },
       {
         backgroundColor: "#00000050",
-        backgroundFilter: "blur(10px)",
+        backdropFilter: "blur(10px)",
         duration: 1,
         ease: "power1.inOut",
       },
