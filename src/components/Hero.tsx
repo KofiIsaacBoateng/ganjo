15aa3a14a30b6d670603484c11ccd30159bf9e3c
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2026-03-28 02:50:46 +0000

    about section half complete

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,38 +1,55 @@
-const Hero = () => {
-  return (
-    <section id="hero" className="noisy">
-      <h1 className="title">MOJITO</h1>
-      <img
-        src="/images/hero-left-leaf.png"
-        className="left-leaf"
-        alt="left leaf"
-      />
-      <img
-        src="/images/hero-right-leaf.png"
-        className="right-leaf"
-        alt="right leaf"
-      />
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
-      <div className="body">
-        <div className="content">
-          <div className="space-y-5 hidden md:block">
-            <p>Cool. Crisps. Classic.</p>
+const Hero = () => {
+  const videoRef = useRef(null);
+  const isMobile = useMediaQuery({ maxWidth: "768px" });
+
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">MOJITO</h1>
+        <img
+          src="/images/hero-left-leaf.png"
+          className="left-leaf"
+          alt="left leaf"
+        />
+        <img
+          src="/images/hero-right-leaf.png"
+          className="right-leaf"
+          alt="right leaf"
+        />
+
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisps. Classic.</p>
+              <p className="subtitle">
+                Sip the Spirit <br /> of Summer
+              </p>
+            </div>
+          </div>
+
+          <div className="view-cocktails">
             <p className="subtitle">
-              Sip the Spirit <br /> of Summer
+              Every cocktail on our menu is a blend of premium ingredients,
+              creative flair, and timeless recipes - designed to delight your
+              senses.
             </p>
+            <a href="#cocktails">View Cocktails</a>
           </div>
         </div>
-
-        <div className="view-cocktails">
-          <p className="subtitle">
-            Every cocktail on our menu is a blend of premium ingredients,
-            creative flair, and timeless recipes - designed to delight your
-            senses.
-          </p>
-          <a href="#cocktails">View Cocktails</a>
-        </div>
+      </section>
+      <div className="video absolute inset-0">
+        <video
+          src="/videos/output.mp4"
+          playsInline
+          muted
+          preload="auto"
+          ref={videoRef}
+        />
       </div>
-    </section>
+    </>
   );
 };
 
